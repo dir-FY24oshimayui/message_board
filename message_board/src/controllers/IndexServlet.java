9cc2c8a7f6e671aee4ad50837ec08211951418a9
dir-FY24oshimayui <yui.oshima.z029@dirbato.co.jp>
2024-04-26 09:56:53 +0900

    フラッシュメッセージを追加

--- message_board/src/controllers/IndexServlet.java
+++ message_board/src/controllers/IndexServlet.java
@@ -39,8 +39,12 @@ public class IndexServlet extends HttpServlet {
 
         request.setAttribute("messages", messages);
 
-        var rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
-        rd.forward(request, response);
+     // フラッシュメッセージがセッションスコープにセットされていたら
+        // リクエストスコープに保存する（セッションスコープからは削除）
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
     }
 
+    }
     }
